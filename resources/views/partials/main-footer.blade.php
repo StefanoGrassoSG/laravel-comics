a0dfec74a6a806b99ca4778cb04447fee2a51adb
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-10 12:45:44 +0200

    ADD footer-bootm

--- resources/views/partials/main-footer.blade.php
+++ resources/views/partials/main-footer.blade.php
@@ -19,5 +19,23 @@
         <div class="logo-big">
             <img src="{{ Vite::asset('resources/img/logodc.png') }}" class="img-fluid" alt="">
         </div>
-    </div>  
+    </div> 
+    
+    <div class="footer-bottom">
+        <div class="container-bottom">
+            <div class="sign">
+                <a href="">
+                    <h3>
+                        SIGN-UP NOW!
+                    </h3>
+                </a>
+            </div>
+            <div class="social">
+                <h3>
+                    FOLLOW US
+                </h3>
+    
+            </div>
+        </div>
+    </div>
 </footer>
